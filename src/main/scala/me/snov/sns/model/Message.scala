0737e1fb7eee6d221a1557ed87747a9bf0c46500
Anders Holmberg Bååth <anders.holmbergbaath@aftonbladet.se>
2016-11-14 15:38:18 +0100

    very dirty replacement of uuid with a static atomic long counter, to predict the message id from consumer.

--- src/main/scala/me/snov/sns/model/Message.scala
+++ src/main/scala/me/snov/sns/model/Message.scala
@@ -1,7 +1,11 @@
 package me.snov.sns.model
 
-import java.util.UUID
+import java.util.concurrent.atomic.AtomicLong
 
 case class Message(body: String) {
-  val uuid = UUID.randomUUID()
+  val uuid = Message.counter.getAndIncrement()
+}
+
+object Message {
+  val counter = new AtomicLong(0)
 }
